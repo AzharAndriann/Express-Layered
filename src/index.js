cb6c3e34cfa02ef771645e9cc287b439368bcdd2
unknown <azhardwiandrian@gmail.com>
2024-10-24 09:55:52 +0700

    yang tadi error dikit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-const express = require('express')
+const express = require("express")
+const dotenv = require("dotenv")
 const app = express()
-const port = 3000
 
-app.listen(port, () => console.log(`Dolphin app listening on port ${port}!`))
+dotenv.config()
+
+const PORT = process.env.PORT
 
 app.get("/api", (req, res) => {
     res.send("Hello World")
+})
+
+app.listen(PORT, () => {
+    console.log("Expres API running in port: " + PORT)
 })
